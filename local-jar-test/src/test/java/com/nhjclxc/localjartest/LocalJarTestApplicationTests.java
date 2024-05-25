e68033cfe4893329dfe4438e19cbb68c8c74ff86
LuoXianchao <nhjclxc@163.com>
2024-05-25 16:33:34 +0800

    feat：local-jar-test

---
+++ local-jar-test/src/test/java/com/nhjclxc/localjartest/LocalJarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhjclxc.localjartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalJarTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
